54a14e646428fc6063f47b753dbe428561d42212
Josiah Guevarra <701031808@GoTVDSB.ca>
2025-03-19 14:09:10 -0400

    sadsdsd

--- Program.cs
+++ Program.cs
@@ -10,41 +10,44 @@ namespace _6
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.Magenta;
             Console.Clear();
-            
+
             Random generator = new Random();
             int randNumb;
 
-//            randNumb = generator.Next(10);
 
-//            Console.WriteLine("My random number is " + randNumb);
-//            Console.WriteLine("Here are some numbers from 0 to 4!");
-//            Console.Write(generator.Next(5) + " ");
-//            Console.Write(generator.Next(5) + " ");
-//            Console.Write(generator.Next(5) + " ");
-//            Console.Write(generator.Next(5) + " ");
-//            Console.Write(genera  Console.ReadLine(); // Keeps the program from quitting
-//                                                     
-//            Console.WriteLine(generator.Next(5) + " ");
-//            Console.WriteLine();
-//            Console.WriteLine("Here are some numbers from 0 to 99!");
-//            Console.Write(generator.Next(100) + " ");
-//            Console.Write(generator.Next(100) + " ");
-//            Console.Write(generator.Next(100) + " ");
-//            Console.Write(generator.Next(100) + " ");
-//            Console.Write(generator.Next(100) + " ");
-//            Console.WriteLine(generator.Next(100) + " ");
-//            Console.WriteLine();
-//            int num1 = generator.Next(10);
-//            int num2 = generator.Next(10);
-//            if (num1 == num2)
-//            {
-//                Console.WriteLine("The random numbers were the same! Weird.");
-//            }
-//            if (num1 != num2)
-//            {
-//                Console.WriteLine("The random numbers were different! Not weird. ");
-//            }
-          
+
+
+            //            randNumb = generator.Next(10);
+
+            //            Console.WriteLine("My random number is " + randNumb);
+            //            Console.WriteLine("Here are some numbers from 0 to 4!");
+            //            Console.Write(generator.Next(5) + " ");
+            //            Console.Write(generator.Next(5) + " ");
+            //            Console.Write(generator.Next(5) + " ");
+            //            Console.Write(generator.Next(5) + " ");
+            //            Console.Write(genera  Console.ReadLine(); // Keeps the program from quitting
+            //                                                     
+            //            Console.WriteLine(generator.Next(5) + " ");
+            //            Console.WriteLine();
+            //            Console.WriteLine("Here are some numbers from 0 to 99!");
+            //            Console.Write(generator.Next(100) + " ");
+            //            Console.Write(generator.Next(100) + " ");
+            //            Console.Write(generator.Next(100) + " ");
+            //            Console.Write(generator.Next(100) + " ");
+            //            Console.Write(generator.Next(100) + " ");
+            //            Console.WriteLine(generator.Next(100) + " ");
+            //            Console.WriteLine();
+            //            int num1 = generator.Next(10);
+            //            int num2 = generator.Next(10);
+            //            if (num1 == num2)
+            //            {
+            //                Console.WriteLine("The random numbers were the same! Weird.");
+            //            }
+            //            if (num1 != num2)
+            //            {
+            //                Console.WriteLine("The random numbers were different! Not weird. ");
+            //            }
+
 
 
 
@@ -54,6 +57,7 @@ namespace _6
             secertNumber = generator.Next(1, 11);
             Console.WriteLine("Welcom to the new guessing game called the Better worst geussing game!");
             Console.WriteLine(" please give me a number from 1-10");
+
             int.TryParse(Console.ReadLine(), out guess);
             if (guess == secertNumber)
             {
@@ -64,6 +68,41 @@ namespace _6
                 Console.WriteLine("your are incorect");
             }
             Console.ReadLine();
+
+
+
+            int resp = generator.Next(5);
+
+            Console.WriteLine(" I see the futre, ask me anything");
+            Console.ReadLine();
+            Thread.Sleep(500);
+            if (resp == 0)
+            {
+                Console.WriteLine("Fat chance");
+            }
+            if (resp == 1)
+            {
+                Console.WriteLine(" I dont know, maybe");
+            }
+            if (resp == 2)
+            {
+                Console.WriteLine("No way!");
+
+            }
+            if(resp == 3)
+            {
+                Console.WriteLine("Positive");
+            }
+            if (resp == 4)
+            {
+                Console.WriteLine("YES");
+            }
+            if (resp == 5)
+            {
+                Console.WriteLine("At some point... just kidding, no");
+            }
+                
+
         }
-    }
-}
+        }
+    } 
